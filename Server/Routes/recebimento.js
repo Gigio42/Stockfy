99b1b3ce2247838d0187534f6afde808ca4e657b
Gigio42 <gigio.42@gmail.com>
2024-04-20 21:09:48 -0300

    transição do express para o fastfy

--- Server/Routes/recebimento.js
+++ Server/Routes/recebimento.js
@@ -1,24 +1,22 @@
-const express = require('express');
-const db = require('../Database');
-
-const router = express.Router();
-
-router.post('/', async (request, response) => {
-    console.log(request.body);
-
-    let insert = 'INSERT INTO produtos (qualidade, medida, quantidade, vincos) VALUES (?,?,?,?)';
-    
-    const promises = request.body.map(item => {
+module.exports = async function(fastify, options) {
+    const db = options.db;
+  
+    fastify.post('/', async (request, reply) => {
+      console.log(request.body);
+  
+      let insert = 'INSERT INTO produtos (qualidade, medida, quantidade, vincos) VALUES (?,?,?,?)';
+  
+      const promises = request.body.map(item => {
         const [qualidade, medida, quantidade, vincos] = item;
         return db.run(insert, [qualidade, medida, quantidade, vincos]);
-    });
-    try {
+      });
+  
+      try {
         await Promise.all(promises);
-        response.json({ message: 'Data received and inserted into SQLite database successfully' });
-    } catch (err) {
+        reply.send({ message: 'Data received and inserted into SQLite database successfully' });
+      } catch (err) {
         console.log(err.message);
-        response.status(500).json({ message: 'Error inserting data into SQLite database', error: err.message });
-    }
-});
-
-module.exports = router;
+        reply.code(500).send({ message: 'Error inserting data into SQLite database', error: err.message });
+      }
+    });
+  };
