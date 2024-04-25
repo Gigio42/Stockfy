fb12ae0b4ba289ba72b0a0a4a94107dccc4bdb9e
joão Luccas <joaoluccas637@gmail.com>
2024-04-24 21:23:31 -0300

    compras, login e pcp

---
+++ Client/login/login.js
@@ -0,0 +1,6 @@
+document.getElementById('login').addEventListener('click', function(event) {
+    event.preventDefault();
+    //@Gigio42 Aqui você pode adicionar a lógica para conectar ao backend
+    // Você pode usar o fetch API para fazer uma solicitação POST ao seu servidor, ou como preferir
+    // fetch('url_do_seu_servidor', { method: 'POST', body: JSON.stringify({ username: uname, password: psw }) })
+});
