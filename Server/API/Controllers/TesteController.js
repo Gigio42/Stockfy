2427e5b605aa3e164ee7876ba0326b4af3e6ba67
Gigio42 <gigio.42@gmail.com>
2024-05-05 00:18:00 -0300

    Colocando um end point de teste para ver os itens adicionados por meio do servidor remoto

---
+++ Server/API/Controllers/TesteController.js
@@ -0,0 +1,13 @@
+import { getRepository } from 'typeorm';
+import Chapas from '../Models/Chapas.js';
+
+class TesteController {
+    constructor() {}
+
+    async getAll(data) {
+        const chapasRepository = getRepository(Chapas);
+        return await chapasRepository.find();
+    }
+}
+
+export default TesteController;
