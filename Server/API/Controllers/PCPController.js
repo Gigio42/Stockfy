3a6d40c4777abb5639c13f3b3764781f8d7bd599
Gigio42 <gigio.42@gmail.com>
2024-05-25 02:55:56 -0300

    PCP integrado ao prisma

--- Server/API/Controllers/PCPController.js
+++ Server/API/Controllers/PCPController.js
@@ -1,167 +1,198 @@
-// import { getRepository } from "typeorm";
-// import Chapas from "../Models/Chapas.js";
-// import Item from "../Models/Item.js";
-// import Chapa_Item from "../Models/Chapa_Item.js";
+import { PrismaClient } from "@prisma/client";
 
-// class PCPController {
-//   constructor() {}
+const prisma = new PrismaClient();
 
-//   // ------------------------------
-//   // GetChapasComment Function
-//   // ------------------------------
-//   async getChapas(query, filterCriteria, sortOrder, sortBy) {
-//     const chapasRepository = getRepository(Chapas);
-//     let data = await chapasRepository.find({ relations: ["conjugacoes"] });
+class PCPController {
+  constructor() {}
 
-//     data = data.filter((chapa) => chapa.status !== "USADO");
+  // ------------------------------
+  // GetChapasComment Function
+  // ------------------------------
+  async getChapas(query, filterCriteria, sortOrder, sortBy) {
+    let data = await prisma.chapas.findMany({ include: { conjugacoes: true } });
 
-//     if (filterCriteria) {
-//       for (let key in filterCriteria) {
-//         if (key === "comprimento" || key === "largura") {
-//           data = data.filter((chapa) => {
-//             const [comprimento, largura] = chapa.medida.split("x");
-//             if (key === "comprimento") {
-//               return comprimento === filterCriteria[key];
-//             } else {
-//               return largura === filterCriteria[key];
-//             }
-//           });
-//         } else {
-//           data = data.filter((chapa) => chapa[key].toLowerCase() === filterCriteria[key].toLowerCase());
-//         }
-//       }
-//     }
+    data = data.filter((chapa) => chapa.status !== "USADO");
 
-//     const sortedChapas = data.sort((a, b) => {
-//       const getValue = (obj, prop) => prop.split(".").reduce((acc, part) => acc && acc[part], obj);
+    if (filterCriteria) {
+      for (let key in filterCriteria) {
+        if (key === "comprimento" || key === "largura") {
+          data = data.filter((chapa) => {
+            const [comprimento, largura] = chapa.medida.split("x");
+            if (key === "comprimento") {
+              return comprimento === filterCriteria[key];
+            } else {
+              return largura === filterCriteria[key];
+            }
+          });
+        } else {
+          data = data.filter((chapa) => chapa[key].toLowerCase() === filterCriteria[key].toLowerCase());
+        }
+      }
+    }
 
-//       if (sortOrder === "asc") {
-//         return getValue(a, sortBy) < getValue(b, sortBy) ? -1 : getValue(a, sortBy) > getValue(b, sortBy) ? 1 : 0;
-//       } else {
-//         return getValue(a, sortBy) > getValue(b, sortBy) ? -1 : getValue(a, sortBy) < getValue(b, sortBy) ? 1 : 0;
-//       }
-//     });
-//     return sortedChapas;
-//   }
+    const sortedChapas = data.sort((a, b) => {
+      const getValue = (obj, prop) => prop.split(".").reduce((acc, part) => acc && acc[part], obj);
 
-//   // ------------------------------
-//   // GetItemsComment Function
-//   // ------------------------------
-//   async getItems(searchQuery = "") {
-//     const chapaItemRepository = getRepository(Chapa_Item);
+      if (sortOrder === "asc") {
+        return getValue(a, sortBy) < getValue(b, sortBy) ? -1 : getValue(a, sortBy) > getValue(b, sortBy) ? 1 : 0;
+      } else {
+        return getValue(a, sortBy) > getValue(b, sortBy) ? -1 : getValue(a, sortBy) < getValue(b, sortBy) ? 1 : 0;
+      }
+    });
+    return sortedChapas;
+  }
 
-//     const chapaItems = await chapaItemRepository
-//       .createQueryBuilder("chapa_item")
-//       .leftJoinAndSelect("chapa_item.item", "item")
-//       .leftJoinAndSelect("chapa_item.chapa", "chapa")
-//       .where("item.part_number LIKE :search", { search: `%${searchQuery}%` })
-//       .getMany();
+  // ------------------------------
+  // GetItemsComment Function
+  // ------------------------------
+  async getItems(searchQuery = "") {
+    const chapaItems = await prisma.chapa_Item.findMany({
+      where: {
+        item: {
+          part_number: {
+            contains: searchQuery,
+          },
+        },
+      },
+      include: {
+        item: true,
+        chapa: true,
+      },
+    });
 
-//     if (!chapaItems.length) {
-//       throw new Error(`No Chapa_Item found`);
-//     }
+    if (!chapaItems.length) {
+      throw new Error(`No Chapa_Item found`);
+    }
 
-//     const items = chapaItems.reduce((acc, chapaItem) => {
-//       const { item } = chapaItem;
-//       if (!acc[item.id_item]) {
-//         acc[item.id_item] = {
-//           ...item,
-//           chapas: [],
-//         };
-//       }
-//       acc[item.id_item].chapas.push(chapaItem.chapa);
-//       return acc;
-//     }, {});
+    const items = chapaItems.reduce((acc, chapaItem) => {
+      const { item } = chapaItem;
+      if (!acc[item.id_item]) {
+        acc[item.id_item] = {
+          ...item,
+          chapas: [],
+        };
+      }
+      acc[item.id_item].chapas.push(chapaItem.chapa);
+      return acc;
+    }, {});
 
-//     return Object.values(items);
-//   }
+    return Object.values(items);
+  }
 
-//   // ------------------------------
-//   // PostItemsComment Function
-//   // ------------------------------
-//   async createItemWithChapa(body) {
-//     const { partNumber, chapas } = body;
+  // ------------------------------
+  // PostItemsComment Function
+  // ------------------------------
+  async createItemWithChapa(body) {
+    const { partNumber, chapas } = body;
 
-//     const chapasRepository = getRepository(Chapas);
-//     const itemRepository = getRepository(Item);
-//     const chapaItemRepository = getRepository(Chapa_Item);
+    let item;
+    let chapasToSave = [];
+    let chapaItemsToSave = [];
 
-//     let item;
-//     let chapasToSave = [];
-//     let chapaItemsToSave = [];
+    for (const { chapaID, quantity, medida, keepRemaining } of chapas) {
+      const chapa = await prisma.chapas.findUnique({ where: { id_chapa: chapaID } });
 
-//     for (const { chapaID, quantity, medida, keepRemaining } of chapas) {
-//       const chapa = await chapasRepository.findOne({ where: { id_chapa: chapaID } });
+      if (!chapa) throw new Error("Chapa not found");
+      if (!quantity) throw new Error("Quantity is required");
+      if (quantity > chapa.quantidade_comprada) throw new Error("Insufficient chapas");
 
-//       if (!chapa) throw new Error("Chapa not found");
-//       if (!quantity) throw new Error("Quantity is required");
-//       if (quantity > chapa.quantidade_comprada) throw new Error("Insufficient chapas");
+      if (!item) {
+        item = await prisma.item.findUnique({ where: { part_number: partNumber } });
 
-//       if (!item) {
-//         item = await itemRepository.findOne({ where: { part_number: partNumber } });
+        if (!item) {
+          item = await prisma.item.create({
+            data: {
+              part_number: partNumber,
+              status: "RESERVADO",
+            },
+          });
+        }
+      }
 
-//         if (!item) {
-//           item = itemRepository.create({
-//             part_number: partNumber,
-//             status: "RESERVADO",
-//           });
+      await prisma.chapas.update({
+        where: { id_chapa: chapa.id_chapa },
+        data: { quantidade_estoque: { decrement: parseInt(quantity) } },
+      });
 
-//           await itemRepository.save(item);
-//         }
-//       }
+      if (chapa.quantidade_comprada + chapa.quantidade_estoque === 0 || (chapa.status === "recebido" && chapa.quantidade_estoque === 0)) {
+        chapa.status = "USADO";
+      }
 
-//       chapa.quantidade_estoque -= quantity;
+      if (keepRemaining) {
+        const [chapaWidth, chapaHeight] = chapa.medida.split("x").map(Number);
+        const [chosenWidth, chosenHeight] = medida.split("x").map(Number);
 
-//       if (chapa.quantidade_comprada + chapa.quantidade_estoque === 0 || (chapa.status === "recebido" && chapa.quantidade_estoque === 0)) {
-//         chapa.status = "USADO";
-//       }
+        if (chapaWidth < chosenWidth || chapaHeight < chosenHeight) {
+          throw new Error("Not enough chapas of the specified dimensions");
+        }
 
-//       if (keepRemaining) {
-//         const [chapaWidth, chapaHeight] = chapa.medida.split("x").map(Number);
-//         const [chosenWidth, chosenHeight] = medida.split("x").map(Number);
+        const originalArea = chapaWidth * chapaHeight;
+        const usedArea = chosenWidth * chosenHeight;
+        const remainingArea = originalArea - usedArea;
 
-//         if (chapaWidth < chosenWidth || chapaHeight < chosenHeight) {
-//           throw new Error("Not enough chapas of the specified dimensions");
-//         }
+        const { id_chapa, medida, ...chapaProps } = chapa;
 
-//         const originalArea = chapaWidth * chapaHeight;
-//         const usedArea = chosenWidth * chosenHeight;
-//         const remainingArea = originalArea - usedArea;
+        const newChapa = await prisma.chapas.create({
+          data: {
+            ...chapaProps,
+            area: remainingArea,
+            quantidade_estoque: quantity,
+            status: "RESTO",
+          },
+        });
 
-//         const { id_chapa, medida, ...chapaProps } = chapa;
+        chapasToSave.push(newChapa);
+      }
 
-//         const newChapa = chapasRepository.create({
-//           ...chapaProps,
-//           area: remainingArea,
-//           quantidade_estoque: quantity,
-//           status: "RESTO",
-//         });
+      chapasToSave.push(chapa);
 
-//         await chapasRepository.save(newChapa);
-//       }
+      let chapaItem = await prisma.chapa_Item.findFirst({
+        where: {
+          AND: [{ chapa: { id_chapa: chapaID } }, { item: { id_item: item.id_item } }],
+        },
+      });
 
-//       chapasToSave.push(chapa);
+      if (chapaItem) {
+        chapaItem.quantidade += Number(quantity);
+      } else {
+        chapaItem = await prisma.chapa_Item.create({
+          data: {
+            quantidade: Number(quantity),
+            chapa: { connect: { id_chapa: chapaID } },
+            item: { connect: { id_item: item.id_item } },
+          },
+        });
+      }
 
-//       let chapaItem = await chapaItemRepository.findOne({ where: { chapa: chapa, item: item } });
+      chapaItemsToSave.push(chapaItem);
+    }
 
-//       if (chapaItem) {
-//         chapaItem.quantidade += Number(quantity);
-//       } else {
-//         chapaItem = chapaItemRepository.create({
-//           chapa: chapa,
-//           item: item,
-//           quantidade: Number(quantity),
-//         });
-//       }
+    for (const chapa of chapasToSave) {
+      await prisma.chapas.update({
+        where: { id_chapa: chapa.id_chapa },
+        data: { quantidade_estoque: { decrement: chapa.quantidade_estoque } },
+      });
+    }
 
-//       chapaItemsToSave.push(chapaItem);
-//     }
+    for (const chapaItem of chapaItemsToSave) {
+      const existingChapaItem = await prisma.chapa_Item.findUnique({
+        where: { id_chapa_item: chapaItem.id_chapa_item },
+      });
 
-//     await chapasRepository.save(chapasToSave);
-//     await chapaItemRepository.save(chapaItemsToSave);
+      if (existingChapaItem) {
+        await prisma.chapa_Item.update({
+          where: { id_chapa_item: chapaItem.id_chapa_item },
+          data: { quantidade: { increment: chapaItem.quantidade } },
+        });
+      } else {
+        await prisma.chapa_Item.create({
+          data: chapaItem,
+        });
+      }
+    }
 
-//     return item;
-//   }
-// }
+    return item;
+  }
+}
 
-// export default PCPController;
+export default PCPController;
--- Server/API/Routes/index.js
+++ Server/API/Routes/index.js
@@ -1,15 +1,15 @@
-import comprasRoute from './comprasRoute.js';
-import recebimentoRoute from './recebimentoRoute.js';
-// import pcpRoute from './PCPRoute.js';
+import comprasRoute from "./comprasRoute.js";
+import recebimentoRoute from "./recebimentoRoute.js";
+import pcpRoute from "./PCPRoute.js";
 // import admRoute from './ADMRoute.js';
-//import producaoRoute from './producaoRoute.js'; 
+// import producaoRoute from './producaoRoute.js';
 // import testeRoute from "./TesteRoute.js";
 
-export default function(fastify) {
-  fastify.register(comprasRoute, { prefix: '/compras' });
-  fastify.register(recebimentoRoute, { prefix: '/recebimento' });
-  // fastify.register(pcpRoute, { prefix: '/PCP' });
+export default function (fastify) {
+  fastify.register(comprasRoute, { prefix: "/compras" });
+  fastify.register(recebimentoRoute, { prefix: "/recebimento" });
+  fastify.register(pcpRoute, { prefix: "/PCP" });
   // fastify.register(admRoute, { prefix: '/adm' });
-  // //fastify.register(producaoRoute, { prefix: '/producao' }); 
+  // //fastify.register(producaoRoute, { prefix: '/producao' });
   // fastify.register(testeRoute, { prefix: '/teste' });
 }
