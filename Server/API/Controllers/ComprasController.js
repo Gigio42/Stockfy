3c60752db6420cc297994a1fb2a98778b09be83c
Gigio42 <gigio.42@gmail.com>
2024-05-26 21:13:54 -0300

    mudando a medida na tb de chapas gradualmente, organização

--- Server/API/Controllers/ComprasController.js
+++ Server/API/Controllers/ComprasController.js
@@ -5,9 +5,28 @@ const prisma = new PrismaClient();
 class ComprasController {
   constructor() {}
 
+  extractDimensions(chapa) {
+    if (chapa.medida) {
+      const dimensions = chapa.medida
+        .toLowerCase()
+        .replace(/\s/g, "")
+        .split("x")
+        .map((dim) => parseFloat(dim.replace(",", ".").replace(".", "")));
+      if (dimensions.length === 2) {
+        chapa.largura = dimensions[0];
+        chapa.comprimento = dimensions[1];
+      }
+    }
+    return chapa;
+  }
+
   async createCompra(orderData) {
-    const promises = orderData.info_prod_comprados.map(async (item) => {
-      return prisma.chapas.create({ data: item });
+    const promises = orderData.info_prod_comprados.map(async (chapa) => {
+      chapa.quantidade_disponivel = chapa.quantidade_comprada;
+
+      chapa = this.extractDimensions(chapa);
+
+      return prisma.chapas.create({ data: chapa });
     });
 
     return await Promise.all(promises);
---
+++ prisma/migrations/20240526234153_problemas_medida/migration.sql
@@ -0,0 +1,3 @@
+-- AlterTable
+ALTER TABLE "Chapas" ADD COLUMN "altura" INTEGER;
+ALTER TABLE "Chapas" ADD COLUMN "largura" INTEGER;
