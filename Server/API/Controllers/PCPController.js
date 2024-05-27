96572c878bd7832def4b278f154ff541d4d85828
Gigio42 <gigio.42@gmail.com>
2024-05-26 22:34:36 -0300

    ordenar por medida e largura + block todo na reciclagem

--- Server/API/Controllers/PCPController.js
+++ Server/API/Controllers/PCPController.js
@@ -86,75 +86,77 @@ class PCPController {
   // PostItemsComment Function
   // ------------------------------
   async createItemWithChapa(body) {
-  const { partNumber, chapas } = body;
+    console.log(body);
+    const { partNumber, chapas } = body;
 
-  for (const { quantity } of chapas) {
-    if (!quantity) throw new Error("Todas as chapas devem ter uma quantidade");
-  }
+    for (const { quantity, keepRemaining } of chapas) {
+      if (keepRemaining) throw new Error("Reciclagem ainda está sendo desenvolvida"); //TODO
+      if (!quantity) throw new Error("Todas as chapas devem ter uma quantidade");
+    }
 
-  let item;
+    let item;
 
-  for (const { chapaID, quantity } of chapas) {
-    const chapa = await prisma.chapas.findUnique({ where: { id_chapa: chapaID } });
+    for (const { chapaID, quantity } of chapas) {
+      const chapa = await prisma.chapas.findUnique({ where: { id_chapa: chapaID } });
 
-    if (!chapa) throw new Error("Chapa não encontrada");
-    if (!quantity) throw new Error("Informe a quantidade de chapas a serem reservadas");
-    if (quantity > chapa.quantidade_disponivel) throw new Error("Chapas insuficientes");
-
-    if (!item) {
-      item = await prisma.item.findUnique({ where: { part_number: partNumber } });
+      if (!chapa) throw new Error("Chapa não encontrada");
+      if (!quantity) throw new Error("Informe a quantidade de chapas a serem reservadas");
+      if (quantity > chapa.quantidade_disponivel) throw new Error("Chapas insuficientes");
 
       if (!item) {
-        item = await prisma.item.create({
+        item = await prisma.item.findUnique({ where: { part_number: partNumber } });
+
+        if (!item) {
+          item = await prisma.item.create({
+            data: {
+              part_number: partNumber,
+              status: "RESERVADO",
+            },
+          });
+        } else if (item.status !== "RESERVADO") {
+          throw new Error("Item em processo");
+        }
+      }
+
+      const updatedChapa = await prisma.chapas.update({
+        where: { id_chapa: chapa.id_chapa },
+        data: {
+          quantidade_disponivel: { decrement: parseInt(quantity) },
+          quantidade_estoque: { decrement: parseInt(quantity) },
+        },
+      });
+
+      if (updatedChapa.quantidade_disponivel === 0) {
+        await prisma.chapas.update({
+          where: { id_chapa: chapa.id_chapa },
+          data: { status: "USADO" },
+        });
+      }
+
+      let chapaItem = await prisma.chapa_Item.findFirst({
+        where: {
+          AND: [{ chapa: { id_chapa: chapaID } }, { item: { id_item: item.id_item } }],
+        },
+      });
+
+      if (chapaItem) {
+        await prisma.chapa_Item.update({
+          where: { id_chapa_item: chapaItem.id_chapa_item },
+          data: { quantidade: { increment: Number(quantity) } },
+        });
+      } else {
+        await prisma.chapa_Item.create({
           data: {
-            part_number: partNumber,
-            status: "RESERVADO",
+            quantidade: Number(quantity),
+            chapa: { connect: { id_chapa: chapaID } },
+            item: { connect: { id_item: item.id_item } },
           },
         });
-      } else if (item.status !== "RESERVADO") {
-        throw new Error("Item em processo");
       }
     }
 
-    const updatedChapa = await prisma.chapas.update({
-      where: { id_chapa: chapa.id_chapa },
-      data: { 
-        quantidade_disponivel: { decrement: parseInt(quantity) },
-        quantidade_estoque: { decrement: parseInt(quantity) }
-      },
-    });
-
-    if (updatedChapa.quantidade_disponivel === 0) {
-      await prisma.chapas.update({
-        where: { id_chapa: chapa.id_chapa },
-        data: { status: "USADO" },
-      });
-    }
-
-    let chapaItem = await prisma.chapa_Item.findFirst({
-      where: {
-        AND: [{ chapa: { id_chapa: chapaID } }, { item: { id_item: item.id_item } }],
-      },
-    });
-
-    if (chapaItem) {
-      await prisma.chapa_Item.update({
-        where: { id_chapa_item: chapaItem.id_chapa_item },
-        data: { quantidade: { increment: Number(quantity) } },
-      });
-    } else {
-      await prisma.chapa_Item.create({
-        data: {
-          quantidade: Number(quantity),
-          chapa: { connect: { id_chapa: chapaID } },
-          item: { connect: { id_item: item.id_item } },
-        },
-      });
-    }
+    return item;
   }
-
-  return item;
-}
 
   // ------------------------------
   // deleteItemComment Function
