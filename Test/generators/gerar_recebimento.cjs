8387d055e28595ddae46170b5f08c70640f13b70
Gigio42 <Gigio42@users.noreply.github.com>
2024-06-01 03:09:11 +0000

    Marvin: formatando arquivos

--- Test/generators/gerar_recebimento.cjs
+++ Test/generators/gerar_recebimento.cjs
@@ -1,12 +1,12 @@
-const axios = require('axios');
-const readline = require('readline');
+const axios = require("axios");
+const readline = require("readline");
 
 const rl = readline.createInterface({
   input: process.stdin,
-  output: process.stdout
+  output: process.stdout,
 });
 
-rl.question('Please enter the id_compra: ', (id_compra) => {
+rl.question("Please enter the id_compra: ", (id_compra) => {
   testRecebimento(id_compra);
   rl.close();
 });
@@ -16,19 +16,19 @@ async function testRecebimento(id_compra) {
     const response = await axios.get(`http://localhost:3000/recebimento/${id_compra}`);
     const chapas = response.data;
 
-    const selectedChapas = chapas.slice(0, 3).map(chapa => chapa.id_produto);
+    const selectedChapas = chapas.slice(0, 3).map((chapa) => chapa.id_produto);
 
     for (let id_produto of selectedChapas) {
       const updateData = {
         id_chapa: id_produto,
         quantidade_recebida: 10,
-        status: 'RECEIVED', 
+        status: "RECEIVED",
       };
 
-      await axios.put('http://localhost:3000/recebimento', updateData);
+      await axios.put("http://localhost:3000/recebimento", updateData);
     }
 
-    console.log('Update successful');
+    console.log("Update successful");
   } catch (error) {
     console.error(error);
   }
