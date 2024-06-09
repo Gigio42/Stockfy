88d5f16ca5cbf168ec81e251510bd1971316fa87
Gigio42 <gigio.42@gmail.com>
2024-06-09 16:39:37 -0300

    Update database provider to PostgreSQL and remove unnecessary migration files

--- prisma/migrations/20240525053819_deixando_partnumber_unico/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - A unique constraint covering the columns `[part_number]` on the table `Item` will be added. If there are existing duplicate values, this will fail.
-
-*/
--- CreateIndex
-CREATE UNIQUE INDEX "Item_part_number_key" ON "Item"("part_number");
--- prisma/migrations/20240526234153_problemas_medida/migration.sql
+++
@@ -1,3 +0,0 @@
--- AlterTable
-ALTER TABLE "Chapas" ADD COLUMN "altura" INTEGER;
-ALTER TABLE "Chapas" ADD COLUMN "largura" INTEGER;
