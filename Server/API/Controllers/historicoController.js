9166a3c024682a6f7a3d41056cf83f6c69b1042d
Luanplays11 <luansouza70690@gmail.com>
2024-07-07 22:13:14 -0300

    pesquisas na pagina do historico

--- Server/API/Controllers/historicoController.js
+++ Server/API/Controllers/historicoController.js
@@ -1,27 +1,39 @@
 import { PrismaClient } from "@prisma/client";
-
 const prisma = new PrismaClient();
 
-class HistoricoController {
-  constructor() {}
-
-  async getChapas() {
-    const chapas = await prisma.Chapas.findMany();
-    return chapas;
-  }
-
-  async getItems() {
-    const items = await prisma.Item.findMany({
-      include: {
-        maquinas: {
-          include: {
-            maquina: true,
-          },
-        },
-      },
-    });
-    return items;
+export async function getAll(request, reply) {
+  try {
+    const data = await prisma.historico.findMany();
+    reply.send(data);
+  } catch (error) {
+    reply.status(500).send({ error: error.message });
   }
 }
 
-export default HistoricoController;
+export async function add(request, reply) {
+  if (!request.body || Object.keys(request.body).length === 0) {
+    return reply.status(400).send({ error: "Dados para criação são necessários." });
+  }
+  
+  try {
+    const entity = await prisma.historico.create({ data: request.body });
+    reply.send(entity);
+  } catch (error) {
+    reply.status(500).send({ error: error.message });
+  }
+}
+
+export async function deleteEntity(request, reply) {
+  const { id } = request.params;
+
+  if (!id) {
+    return reply.status(400).send({ error: "ID é necessário para deletar o registro." });
+  }
+  
+  try {
+    const entity = await prisma.historico.delete({ where: { id_historico: parseInt(id) } });
+    reply.send({ message: "Registro deletado com sucesso." });
+  } catch (error) {
+    reply.status(500).send({ error: error.message });
+  }
+}
