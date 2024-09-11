b346a4c5b18e73d8c0dcd7949ef4c65020b08279
Gigio42 <gigio.42@gmail.com>
2024-09-10 23:29:45 -0300

    mudança para utilizar o back-end da azure

--- Client/utils/config.js
+++ Client/utils/config.js
@@ -1,11 +1,11 @@
 let BASE_URL;
 
+
+
 if (window.location.hostname === "localhost" || window.location.hostname === "127.0.0.1") {
   BASE_URL = "http://localhost:3000";
-} else if (window.location.hostname.includes("serveo.net")) {
-  BASE_URL = "https://reptile-cosmic-firstly.ngrok-free.app";
 } else {
-  BASE_URL = "https://stockfy.onrender.com";
+  BASE_URL = "https://stockfy-dqgudbg4adexbxfw.brazilsouth-01.azurewebsites.net";
 }
 
 export default BASE_URL;
